eaf76e5416ba73182b04afd25cd9af3b071b2e91
Kazumasa Utashiro <kaz@utashiro.com>
2021-02-09 17:37:31 +0900

    minil new App::diff2vba

---
+++ lib/App/diff2vba.pm
@@ -0,0 +1,39 @@
+package App::diff2vba;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::diff2vba - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::diff2vba;
+
+=head1 DESCRIPTION
+
+App::diff2vba is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
